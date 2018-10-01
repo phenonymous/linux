0305b554d9c1379aed6c3ffbd4c9fcc844c3c33d
phenonymous <29523105+phenonymous@users.noreply.github.com>
2018-10-01 14:34:20 -0700

    Update

--- scripts/install-vim-zsh.sh
+++ scripts/install-vim-zsh.sh
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
+# Reset color
+RS="\e[0m"
+# Basic Colors
+BLACK="\e[0;30m"
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+YELLOW="\e[0;33m"
+BLUE="\e[0;34m"
+PURPLE="\e[0;35m"
+CYAN="\e[0;36m"
+WHITE="\e[0;37m"
+
+function_message_title () {
+  echo -e "${CYAN}"
+  echo -e "# | ::::::::::::::::::::::::::::::::::::::::::::: | #"
+  echo -e "# |      ${RS} $1 ${CYAN}"
+  echo -e "# | ::::::::::::::::::::::::::::::::::::::::::::: | #"
+  echo -e "${RS}"
+}
+
+function_check_intall () {
+    type -P $1 &>/dev/null && echo -e  "- Installed - ${GREEN} Ok ${RS} - $1" || echo -e  "- Install - ${RED} No ${RS} - $1"
+}
+
+function_check_intall git
+function_message_title '- **Install Docker**'
 NOW=$(date +%x_%X)
 
 confirm() {
@@ -17,7 +43,7 @@ confirm() {
 
 if ! which git 1> /dev/null; then 
   if confirm "git is requried, install git?"; then
-   sudo apt update && sudo apt install git
+   sudo apt update && sudo apt install git -y
   else
     echo "Aborting.."
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
@@ -26,7 +52,7 @@ fi
 
 if ! which curl 1> /dev/null; then 
   if confirm "curl is requried, install curl?"; then
-   sudo apt update && sudo apt install curl
+   sudo apt update && sudo apt install curl -y
   else
     echo "Aborting.."
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
@@ -37,5 +63,6 @@ if confirm "Do you want to install Vundle?"; then
   echo "Installing vundle..." && \
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-  [[ -f ~/.vimrc ]] && echo "Backing up old .vimrc to ~/.vimrc.old" && mv ~/.vimrc ~/.vimrc.old."$NOW"
+  [[ -f ~/.vimrc ]] && echo "Backing up old .vimrc to ~/.vimrc.old.$NOW" && mv ~/.vimrc ~/.vimrc.old."$NOW"
+
 fi
