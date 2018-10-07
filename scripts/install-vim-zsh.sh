5ddc7a9dff243be155e4bc0742cbadccb0c78fc4
phenonymous <29523105+phenonymous@users.noreply.github.com>
2018-10-07 15:36:32 +0200

    Update install script

--- scripts/install-vim-zsh.sh
+++ scripts/install-vim-zsh.sh
@@ -2,33 +2,83 @@
 
 set -euo pipefail
 
-# Reset color
-RS="\e[0m"
-# Basic Colors
-BLACK="\e[0;30m"
-RED="\e[0;31m"
-GREEN="\e[0;32m"
-YELLOW="\e[0;33m"
-BLUE="\e[0;34m"
-PURPLE="\e[0;35m"
-CYAN="\e[0;36m"
-WHITE="\e[0;37m"
+DEFAULT=$(tput sgr0)
 
-function_message_title () {
-  echo -e "${CYAN}"
-  echo -e "# | ::::::::::::::::::::::::::::::::::::::::::::: | #"
-  echo -e "# |      ${RS} $1 ${CYAN}"
-  echo -e "# | ::::::::::::::::::::::::::::::::::::::::::::: | #"
-  echo -e "${RS}"
+
+# ,------- [ pre ]
+#:
+#:
+
+zshrc[0]="   ███████╗███████╗██╗  ██╗██████╗  ██████╗"
+zshrc[1]="   ╚══███╔╝██╔════╝██║  ██║██╔══██╗██╔════╝"
+zshrc[2]="     ███╔╝ ███████╗███████║██████╔╝██║     "
+zshrc[3]="    ███╔╝  ╚════██║██╔══██║██╔══██╗██║     "
+zshrc[4]="██╗███████╗███████║██║  ██║██║  ██║╚██████╗"
+zshrc[5]="╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝"
+
+vimrc[0]="  ██╗   ██╗██╗███╗   ███╗██████╗  ██████╗"
+vimrc[1]="  ██║   ██║██║████╗ ████║██╔══██╗██╔════╝"
+vimrc[2]="  ██║   ██║██║██╔████╔██║██████╔╝██║     "
+vimrc[3]="  ╚██╗ ██╔╝██║██║╚██╔╝██║██╔══██╗██║     "
+vimrc[4]="██╗╚████╔╝ ██║██║ ╚═╝ ██║██║  ██║╚██████╗"
+vimrc[5]="╚═╝ ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝"
+                                           
+REAL_OFFSET_X=$(tput cols)
+REAL_OFFSET_Y=$(tput lines)
+
+draw_char() {
+  V_COORD_X=$1
+  V_COORD_Y=$2
+
+  tput cup $(((REAL_OFFSET_Y/2) + V_COORD_Y - 3)) $(((REAL_OFFSET_X/2) + V_COORD_X - (LENGTH/2)))
+
+  if [[ "$3" == "outro" ]]; then
+    echo " "
+  else
+    echo -e "${DATA[V_COORD_Y]:V_COORD_X:1}"
+  fi
 }
 
-function_check_intall () {
-    type -P $1 &>/dev/null && echo -e  "- Installed - ${GREEN} Ok ${RS} - $1" || echo -e  "- Install - ${RED} No ${RS} - $1"
-}
+draw_text() {
+  case "$1" in
+    zshrc )
+      DATA=("${zshrc[@]}")  ;;
+    vimrc )
+      DATA=("${vimrc[@]}")  ;;
+    * )
+      echo ;;
+  esac
 
-function_check_intall git
-function_message_title '- **Install Docker**'
-NOW=$(date +%x_%X)
+  LENGTH=${#DATA[0]}
+
+  declare -a rand_order
+
+  for (( i=0; i<${#DATA[@]}; i++ )); do
+    for (( j=0; j<${#DATA[0]}; j++ )); do
+      rand_order+=("$j;$i")
+    done
+  done
+
+  rand_order=($(echo "${rand_order[@]}" | tr " " "\n" | shuf | tr "\n" " "))
+
+  tput civis
+  clear
+
+  for (( i=0; i<${#rand_order[@]}; i++ )); do
+    draw_char ${rand_order[i]/;/ }
+    sleep 0.01s
+  done
+
+  sleep 2s
+
+  for (( i=0; i<${#rand_order[@]}; i++ )); do
+    draw_char ${rand_order[i]/;/ } outro
+    sleep 0.01s
+  done
+
+  tput cnorm
+  clear
+}
 
 confirm() {
   local res
@@ -41,21 +91,23 @@ confirm() {
   fi
 }
 
-if ! which git 1> /dev/null; then 
+NOW=$(date +%x_%X)
+
+if ! which git &> /dev/null; then 
   if confirm "git is requried, install git?"; then
    sudo apt update && sudo apt install git -y
   else
     echo "Aborting.."
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
   fi
 fi
 
-if ! which curl 1> /dev/null; then 
+if ! which curl &> /dev/null; then 
   if confirm "curl is requried, install curl?"; then
    sudo apt update && sudo apt install curl -y
   else
     echo "Aborting.."
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    [[ "$0" = "${BASH_SOURCE[0]}" ]] && exit 1 || return 1
   fi
 fi
 
@@ -64,5 +116,4 @@ if confirm "Do you want to install Vundle?"; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
   [[ -f ~/.vimrc ]] && echo "Backing up old .vimrc to ~/.vimrc.old.$NOW" && mv ~/.vimrc ~/.vimrc.old."$NOW"
-
 fi
